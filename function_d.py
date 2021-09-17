9d05ed5ddb20c3f42bf054377a3fd788ed01bdd1
Megan <megan.hagar@gmail.com>
2021-09-17 14:46:39 -0700

    Meaningful message

--- function_d.py
+++ function_d.py
@@ -3,7 +3,10 @@ def max_value(numbers):
         in the list.
     """
     pass
-
+    max = 0
+    for numner in numbers:
+        if numner > 0:
+            max = numner
 
 if __name__ == "__main__":
     print(max_value([1, 12, 2, 42, 8, 3]))
